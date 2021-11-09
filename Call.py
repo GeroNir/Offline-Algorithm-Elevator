b4b26c9554a02d5d5e7f5c389a1cbaa2d7e0305b
hagai_hen <hagai349@gmail.com>
2021-11-09 19:59:28 +0200

    day1

---
+++ Call.py
@@ -0,0 +1,31 @@
+import csv
+
+time = 0
+src = 0
+dest = 0
+numOfCalls = 0
+status = 0
+
+class Call:
+    def __init__(self, file, callNumber):
+
+        with open(file) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 1
+            for row in csv_reader:
+                if line_count == callNumber:
+                    self.time = row[1]
+                    self.src = row[2]
+                    self.dest = row[3]
+                    line_count += 1
+                else:
+                    line_count += 1
+
+        with open(file) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            row_count = sum(1 for row in csv_reader)
+            self.numOfCalls = row_count
+        if (src > dest):
+            self.status = -1
+        elif (dest > src):
+            self.status = 1
